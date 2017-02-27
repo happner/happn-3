6c6e441636ef1c4ce961c0659d51dec4399f4e5b
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2017-02-27 12:46:22 +0200

    still on ack

--- test/g1_consistency_options.js
+++ test/g1_consistency_options.js
@@ -34,7 +34,7 @@ describe('g1_consistency_options', function () {
     else done();
   });
 
-  it('does a set with a publish, deferred consistency, picks up publication log from client', function (done) {
+  it('does a set with a publish, deferred consistency, picks up publication log from the onPublished event handler', function (done) {
 
     var clientConfig = {};
 
@@ -264,7 +264,7 @@ describe('g1_consistency_options', function () {
 
   });
 
-  it('does a set with a publish, deferred consistency, times the publication acknowledgement out', function (done) {
+  it('does a set with a publish, deferred consistency, times the publication out', function (done) {
 
     this.timeout(10000);
 
@@ -358,7 +358,7 @@ describe('g1_consistency_options', function () {
 
   });
 
-  it.only('does a set with a publish, acknowledged consistency publishResults:true, picks up publication log the set results meta', function (done) {
+  it.only('does a set with a publish, acknowledged consistency, picks up publication log in the onPublished event handler', function (done) {
 
     var clientConfig = {};
 
@@ -375,8 +375,117 @@ describe('g1_consistency_options', function () {
     var subscription1, subscription2;
 
     service.create(config)
+
       .then(function (instance) {
+
         serviceInstance = instance;
+        return Happn.client.create(clientConfig);
+      })
+
+      .then(function (client) {
+
+        clientInstance1 = client;
+        return Happn.client.create(clientConfig);
+      })
+
+      .then(function (client) {
+
+        clientInstance2 = client;
+
+        return clientInstance1.on('/test/path/acknowledged/*', {
+          meta: {publish: true},
+          onPublished:function (data) {
+            ran1 = true;
+          }
+        });
+      })
+
+      .then(function (subscription) {
+
+        subscription1 = subscription;
+
+        return clientInstance2.on('/test/path/acknowledged/*', {
+          meta: {publish: true},
+          onPublished:function (data) {
+            ran2 = true;
+          }
+        });
+      })
+
+      .then(function (subscription) {
+
+        subscription2 = subscription;
+
+        return new Promise(function (resolve, reject) {
+
+          clientInstance1.set('/test/path/acknowledged/1', {test: 'data'}, {
+
+            consistency:CONSISTENCY.ACKNOWLEDGED,
+
+            onPublished:function(e, results){
+
+              if (e) return reject(e);
+
+              resolve(results);
+            }
+          }, function(e, response){
+
+            if (e) return reject(e);
+
+            else resolve(response);
+          })
+        });
+      })
+
+      .then(function (results) {
+
+        console.log('RESULTS ARE:::', results);
+
+        expect(results._meta.publishResults.queued).to.be(2);
+        expect(results._meta.publishResults.successful).to.be(2);
+
+        //TODO: test unsubscribe
+
+        done();
+      })
+      .catch(done)
+
+  });
+
+  it('does a set with a publish, acknowledged consistency, times one of the acknowledgements out', function (done) {
+
+    this.timeout(10000);
+
+    var clientConfig = {};
+
+    var config = {
+      services: {
+        subscription: {
+          config: {}
+        }
+      }
+    };
+
+    var ran1 = false, ran2 = false;
+
+    var subscription1, subscription2;
+
+    var oldPerformPublication;
+
+    service.create(config)
+
+      .then(function (instance) {
+
+        serviceInstance = instance;
+
+        //we overwrite this function - so publish never happens
+        serviceInstance.services.publisher.performPublication = function(publication, callback){
+
+          return callback(null, publication.message);
+        };
+
+        serviceInstance.services.queue.__publicationQueue = async.queue(serviceInstance.services.publisher.performPublication.bind(serviceInstance.services.publisher), 10);
+
         return Happn.client.create(clientConfig);
       })
       .then(function (client) {
@@ -388,7 +497,7 @@ describe('g1_consistency_options', function () {
 
         clientInstance2 = client;
 
-        return clientInstance1.on('/test/path/transactional/*', {
+        return clientInstance1.on('/test/path/acknowledged_timed_out/*', {
           meta: {publish: true},
           onPublished:function (data) {
             ran1 = true;
@@ -399,7 +508,7 @@ describe('g1_consistency_options', function () {
 
         subscription1 = subscription;
 
-        return clientInstance2.on('/test/path/transactional/*', {
+        return clientInstance2.on('/test/path/acknowledged_timed_out/*', {
           meta: {publish: true},
           onPublished:function (data) {
             ran2 = true;
@@ -409,26 +518,29 @@ describe('g1_consistency_options', function () {
       .then(function (subscription) {
 
         subscription2 = subscription;
+        setHappened = false;
 
-        return new Promise(function (resolve, reject) {
+        clientInstance1.set('/test/path/acknowledged_timed_out/1', {test: 'data'}, {
 
-          clientInstance1.set('/test/path/transactional/1', {test: 'data'}, {
-            consistency:CONSISTENCY.ACKNOWLEDGED,
-            publishResults:true
-          }, function(e, response){
-            if (e) return reject(e);
-            else resolve(response);
-          })
-        });
-      })
-      .then(function (results) {
+          consistency:CONSISTENCY.DEFERRED,
 
-        expect(results._meta.publishResults.queued).to.be(2);
-        expect(results._meta.publishResults.successful).to.be(2);
+          onPublishedTimeout:5000,
 
-        //TODO: test unsubscribe
+          onPublished:function(e, results){
 
-        done();
+            if (!e) return done(new Error('should have failed'));
+
+            expect(e.toString()).to.be('Error: publication unacknowledged');
+
+            expect(setHappened).to.be(true);
+
+            done();
+          }
+        }, function(e){
+          if (e) return done(e);
+          setHappened = true;
+        })
+
       })
       .catch(done)
 
