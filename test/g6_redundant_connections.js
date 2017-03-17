3bf8cb3a316aa52f5a4182ef8e72174eccf88386
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2017-03-17 10:32:41 +0200

    test passing, now for negative and variants

--- test/g6_redundant_connections.js
+++ test/g6_redundant_connections.js
@@ -19,6 +19,8 @@ describe('g6_redundant_connections', function () {
   var service2Client;
   var service3Client;
 
+  var redundantClient;
+
   var default_timeout = 4000;
 
   var instances = [];
@@ -27,7 +29,7 @@ describe('g6_redundant_connections', function () {
 
     service1Client.disconnect({timeout:2000})
       .then(service2Client.disconnect({timeout:2000}))
-      .then(defaultClient.disconnect({timeout:2000}))
+      .then(service3Client.disconnect({timeout:2000}))
       .then(function () {
 
         async.eachSeries(instances, function (instance, eachCallback) {
@@ -49,9 +51,9 @@ describe('g6_redundant_connections', function () {
         callback();
       }
     );
-  }
+  };
 
-  it('should initialize the services', function (callback) {
+  before('should initialize the services', function (callback) {
 
     this.timeout(20000);
 
@@ -72,24 +74,100 @@ describe('g6_redundant_connections', function () {
     }
   });
 
-  it('should initialize the client', function (callback) {
-
+  before('should initialize the clients', function (callback) {
     this.timeout(default_timeout);
 
     try {
+      //plugin, config, context,
 
-      happn_client.create([
-        {config: {port: service1Port}},
-        {config: {port: service2Port}},
-        {config: {port: service3Port}}
-      ], {info:'test'}, function (e, instance) {
+      happn_client.create({config: {port: service1Port}}, function (e, instance) {
 
+        if (e) return callback(e);
 
+        service1Client = instance;
+        happn_client.create({config: {port: service2Port}}, function (e, instance) {
 
+          if (e) return callback(e);
+
+          service2Client = instance;
+          happn_client.create({config: {port: service3Port}}, function (e, instance) {
+
+            if (e) return callback(e);
+
+            service3Client = instance;
+            callback();
+
+          });
+        });
       });
+
     } catch (e) {
       callback(e);
     }
   });
 
+  var setData = function(client, id, callback){
+    return client.set('/test/data', {id:id}, callback);
+  };
+
+  before('should set up the test data', function (callback) {
+
+    setData(service1Client, 1, function(e){
+      if (e) return callback(e);
+      setData(service2Client, 2, function(e){
+        if (e) return callback(e);
+        setData(service3Client, 3, callback);
+      });
+    });
+  });
+
+  it('should connect to the first service, then service to be switched off - fallback to second service', function(done){
+
+    this.timeout(5000);
+
+    happn_client.create([
+      {config: {port: service1Port}},
+      {config: {port: service2Port}},
+      {config: {port: service3Port}}
+    ], {
+        info:'test',//info is appended to each connection
+        poolType:happn.constants.CONNECTION_POOL_TYPE.ORDERED,//default is 0 RANDOM
+        poolReconnectAttempts:3, //how many switches to perform until a connection is made
+        socket:{
+          reconnect:{
+            retries:1,//one retry
+            timeout:100
+          }
+        }
+      }, function (e, instance) {
+
+      if (e) return done(e);
+
+      redundantClient = instance;
+
+      redundantClient.onEvent('reconnect-successful', function(data){
+
+        redundantClient.get('/test/data', function(e, response) {
+
+          if (e) return done(e);
+
+          expect(response.id).to.be(2);
+
+          redundantClient.disconnect(done);
+        });
+      });
+
+      redundantClient.get('/test/data', function(e, response){
+
+        if (e) return done(e);
+
+        expect(response.id).to.be(1);
+
+        instances[0].stop(function(e){
+
+          if (e) return done(e);
+        });
+      });
+    });
+  });
 });
