b26e584777541bb0857359f42d210f0965bc55a2
southbite <simon@tenacious.digital>
2017-04-12 11:23:01 +0200

    updated longrunners to go in serial

--- test/test-longrunning/serial-run.js
+++ test/test-longrunning/serial-run.js
@@ -3,23 +3,24 @@ var sm = require("happner-serial-mocha")
   , fs = require("fs")
   ;
 
-var testDir = path.resolve(__dirname, '../');
+var testDir = __dirname;
 
 var files = [];
 
 fs.readdirSync(testDir).forEach(function (filename) {
 
   var filePath = testDir + path.sep + filename;
+
   var file = fs.statSync(filePath);
 
-  if (!file.isDirectory() && filename.indexOf('.js') > -1) files.push(filePath);
+  if (!file.isDirectory() && filename.indexOf('.js') > -1 && filename.indexOf('serial-run') == -1) files.push(filePath);
 });
 
-var reportDir = path.resolve(__dirname, '../reports');
+var reportDir = testDir + path.sep + 'reports';
+
+console.log('about to run:::', reportDir);
 
 sm.runTasks(files, null, reportDir)
-
-  //sm.runTasks(files, 'lib/serialReporter.js', true)
 
   .then(function(results){
     console.log('tests completed, check the latest report file in ' + reportDir);
