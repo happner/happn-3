8fee668b616c4edaa12e8ddcf0993b39a63a08b1
southbite <simon@tenacious.digital>
2018-09-10 11:25:15 +0200

    extended subscription perf tests

--- test/performance/subscriptions.js
+++ test/performance/subscriptions.js
@@ -11,9 +11,9 @@ describe(require('../__fixtures/utils/test_helper').create().testName(__filename
   var shortid = require('shortid');
   var random = require('../__fixtures/utils/random');
 
-  this.timeout(5000);
+  this.timeout(SUBSCRIPTION_COUNT * 100);
 
-  before('should initialize the service', function (callback) {
+  beforeEach('should initialize the service', function (callback) {
     service.create({},
       function (e, happnInst) {
         if (e) return callback(e);
@@ -22,12 +22,8 @@ describe(require('../__fixtures/utils/test_helper').create().testName(__filename
       });
   });
 
-  after(function (done) {
-    happnInstance.stop({}, done);
-  });
-
   var client;
-  before('should initialize the client', function (callback) {
+  beforeEach('should initialize the client', function (callback) {
     happnInstance.services.session.localClient(function (e, instance) {
       if (e) return callback(e);
       client = instance;
@@ -35,28 +31,18 @@ describe(require('../__fixtures/utils/test_helper').create().testName(__filename
     });
   });
 
-  var SUBSCRIPTION_COUNT = 10000;
+  afterEach('disconnect client', function (done) {
+    client.disconnect({}, done);
+  });
+
+  afterEach('disconnect server', function (done) {
+    happnInstance.stop(done);
+  });
+
+  var SUBSCRIPTION_COUNT = 100;
   var SEARCH_COUNT = 10000;
 
-  this.timeout(SUBSCRIPTION_COUNT * 10);
-
-  function verifyResults(searchResults, randomPaths){
-
-    console.log('verifying...');
-
-    var ok = true;
-
-    randomPaths.every(function(path){
-
-      if (searchResults[path] == 0) ok = false;
-
-      return ok;
-    });
-
-    console.log('verified...');
-  }
-
-  it('creates ' + SUBSCRIPTION_COUNT + ' random paths, and randomly selects a wildcard option for each path, subscribes, then loops through the paths and searches ' + SEARCH_COUNT + ' times', function (done) {
+  it('creates ' + SUBSCRIPTION_COUNT + ' random paths, and randomly selects a wildcard option for each path, subscribes, then loops through the paths and searches ' + SEARCH_COUNT + ' times in parallel', function (done) {
 
     var subscriptions = [];
 
@@ -73,6 +59,10 @@ describe(require('../__fixtures/utils/test_helper').create().testName(__filename
       subscriptions.push(subscriptionPath);
     });
 
+    var searchPaths = [];
+
+    for (var i = 0; i < SEARCH_COUNT; i++) searchPaths.push(randomPaths[random.integer(0, randomPaths.length - 1)]);
+
     console.log('built subscriptions...');
 
     var startedSubscribing = Date.now();
@@ -86,7 +76,6 @@ describe(require('../__fixtures/utils/test_helper').create().testName(__filename
       setResults[meta.path].push(data);
       setResults.counters[data.counter] = true;
       eventsCount++;
-
       if (eventsCount == SEARCH_COUNT){
         // console.log(JSON.stringify(setResults, null, 2));
         // console.log(JSON.stringify(subscriptions, null, 2));
@@ -101,12 +90,180 @@ describe(require('../__fixtures/utils/test_helper').create().testName(__filename
       console.log('did ' + SUBSCRIPTION_COUNT + ' subscriptions in ' + ((Date.now() - startedSubscribing) / 1000).toString() + ' seconds');
       startedSearching = Date.now();
       var counter = 0;
-      async.each(randomPaths, function(randomPath, randomPathCB){
+      async.each(searchPaths, function(randomPath, randomPathCB){
         client.set(randomPath, {
           counter:counter++
-        }, randomPathCB);
+        }, {noStore:true}, randomPathCB);
       }, function(e){
         if (e) return done(e);
+        console.log('handled ' + SEARCH_COUNT + ' parallel sets in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
+      });
+    });
+  });
+
+  it('creates ' + SUBSCRIPTION_COUNT + ' random paths, and randomly selects a wildcard option for each path, subscribes, then loops through the paths and searches ' + SEARCH_COUNT + ' times in series', function (done) {
+
+    var subscriptions = [];
+
+    console.log('building subscriptions...');
+
+    var randomPaths = random.randomPaths({count:SUBSCRIPTION_COUNT});
+
+    randomPaths.forEach(function(path){
+
+      var possibleSubscriptions = random.getWildcardPermutations(path);
+
+      var subscriptionPath = possibleSubscriptions[random.integer(0, possibleSubscriptions.length - 1)];
+
+      subscriptions.push(subscriptionPath);
+    });
+
+    var searchPaths = [];
+
+    for (var i = 0; i < SEARCH_COUNT; i++) searchPaths.push(randomPaths[random.integer(0, randomPaths.length - 1)]);
+
+    console.log('built subscriptions...');
+
+    var startedSubscribing = Date.now();
+    var setResults = {counters:{}};
+
+    var eventsCount = 0;
+    var startedSearching;
+
+    var handleOn = function(data, meta){
+      if (!setResults[meta.path]) setResults[meta.path] = [];
+      setResults[meta.path].push(data);
+      setResults.counters[data.counter] = true;
+      eventsCount++;
+      if (eventsCount == SEARCH_COUNT){
+        // console.log(JSON.stringify(setResults, null, 2));
+        // console.log(JSON.stringify(subscriptions, null, 2));
+        console.log('handled ' + SEARCH_COUNT + ' events in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
+        done();
+      }
+    };
+
+    async.eachSeries(subscriptions, function(subscription, subscriptionCB){
+      client.on(subscription, handleOn, subscriptionCB);
+    }, function(e){
+      console.log('did ' + SUBSCRIPTION_COUNT + ' subscriptions in ' + ((Date.now() - startedSubscribing) / 1000).toString() + ' seconds');
+      startedSearching = Date.now();
+      var counter = 0;
+      async.each(searchPaths, function(randomPath, randomPathCB){
+        client.set(randomPath, {
+          counter:counter++
+        }, {noStore:true}, randomPathCB);
+      }, function(e){
+        if (e) return done(e);
+        console.log('handled ' + SEARCH_COUNT + ' consecutive sets in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
+      });
+    });
+  });
+
+  it('creates ' + SUBSCRIPTION_COUNT + ' random paths, subscribes to each path, then loops through the paths and searches ' + SEARCH_COUNT + ' times in parallel', function (done) {
+
+    var subscriptions = [];
+
+    console.log('building subscriptions...');
+
+    var randomPaths = random.randomPaths({count:SUBSCRIPTION_COUNT});
+
+    randomPaths.forEach(function(path){
+      subscriptions.push(path);
+    });
+
+    var searchPaths = [];
+
+    for (var i = 0; i < SEARCH_COUNT; i++) searchPaths.push(randomPaths[random.integer(0, randomPaths.length - 1)]);
+
+    console.log('built subscriptions...');
+
+    var startedSubscribing = Date.now();
+    var setResults = {counters:{}};
+
+    var eventsCount = 0;
+    var startedSearching;
+
+    var handleOn = function(data, meta){
+      if (!setResults[meta.path]) setResults[meta.path] = [];
+      setResults[meta.path].push(data);
+      setResults.counters[data.counter] = true;
+      eventsCount++;
+      if (eventsCount == SEARCH_COUNT){
+        // console.log(JSON.stringify(setResults, null, 2));
+        // console.log(JSON.stringify(subscriptions, null, 2));
+        console.log('handled ' + SEARCH_COUNT + ' events in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
+        done();
+      }
+    };
+
+    async.each(subscriptions, function(subscription, subscriptionCB){
+      client.on(subscription, handleOn, subscriptionCB);
+    }, function(e){
+      console.log('did ' + SUBSCRIPTION_COUNT + ' subscriptions in ' + ((Date.now() - startedSubscribing) / 1000).toString() + ' seconds');
+      startedSearching = Date.now();
+      var counter = 0;
+      async.each(searchPaths, function(randomPath, randomPathCB){
+        client.set(randomPath, {
+          counter:counter++
+        }, {noStore:true}, randomPathCB);
+      }, function(e){
+        if (e) return done(e);
+        console.log('handled ' + SEARCH_COUNT + ' parallel sets in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
+      });
+    });
+  });
+
+  it('creates ' + SUBSCRIPTION_COUNT + ' random paths, subscribes to each path, then loops through the paths and searches ' + SEARCH_COUNT + ' times in series', function (done) {
+
+    var subscriptions = [];
+
+    console.log('building subscriptions...');
+
+    var randomPaths = random.randomPaths({count:SUBSCRIPTION_COUNT});
+
+    randomPaths.forEach(function(path){
+      subscriptions.push(path);
+    });
+
+    var searchPaths = [];
+
+    for (var i = 0; i < SEARCH_COUNT; i++) searchPaths.push(randomPaths[random.integer(0, randomPaths.length - 1)]);
+
+    console.log('built subscriptions...');
+
+    var startedSubscribing = Date.now();
+    var setResults = {counters:{}};
+
+    var eventsCount = 0;
+    var startedSearching;
+
+    var handleOn = function(data, meta){
+      if (!setResults[meta.path]) setResults[meta.path] = [];
+      setResults[meta.path].push(data);
+      setResults.counters[data.counter] = true;
+      eventsCount++;
+      if (eventsCount == SEARCH_COUNT){
+        // console.log(JSON.stringify(setResults, null, 2));
+        // console.log(JSON.stringify(subscriptions, null, 2));
+        console.log('handled ' + SEARCH_COUNT + ' events in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
+        done();
+      }
+    };
+
+    async.eachSeries(subscriptions, function(subscription, subscriptionCB){
+      client.on(subscription, handleOn, subscriptionCB);
+    }, function(e){
+      console.log('did ' + SUBSCRIPTION_COUNT + ' subscriptions in ' + ((Date.now() - startedSubscribing) / 1000).toString() + ' seconds');
+      startedSearching = Date.now();
+      var counter = 0;
+      async.each(searchPaths, function(randomPath, randomPathCB){
+        client.set(randomPath, {
+          counter:counter++
+        }, {noStore:true}, randomPathCB);
+      }, function(e){
+        if (e) return done(e);
+        console.log('handled ' + SEARCH_COUNT + ' consecutive sets in ' + ((Date.now() - startedSearching) / 1000).toString() + ' seconds');
       });
     });
   });
