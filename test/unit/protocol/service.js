3286a15c1f36cfff73f3c7d36c95e377e4c5a193
southbite <simon@tenacious.digital>
2019-01-14 18:13:32 +0200

    update processMessageInLayers no uses __getProtocol

--- test/unit/protocol/service.js
+++ test/unit/protocol/service.js
@@ -203,23 +203,102 @@ describe(require('../../__fixtures/utils/test_helper').create().testName(__filen
     protocolMock.initialize({
     }, function(e){
 
-      protocolMock.processMessageIn = function processMessageIn (message, callback) {
-
-        var _this = this;
-
-        var protocol = _this.config.protocols[message.session.protocol];
-        if (!protocol) return callback(_this.happn.services.error.SystemError('unknown inbound protocol: ' + message.session.protocol, 'protocol'));
-
-        _this.processInboundStack(message, protocol)
-        .then(function (processed) {
-          callback(null, protocol.success(processed));
-        })
-        .catch(function(e){
-          _this.__handleProtocolError(protocol, message, e, callback);
-        });
-      }.bind(protocolMock);
-
       protocolMock.processMessageIn({
+        session:{
+          protocol:'bad'
+        }
+      });
+    });
+  });
+
+  it('tests the processMessageInLayers method, happn_1.3.0 protocol', function(done){
+
+    var protocolMock = new Protocol({logger:{
+      createLogger:function(){
+        return {
+          $$TRACE:function(){}
+        };
+      }
+    }});
+
+    protocolMock.happn = {
+      connect:{},
+      log:{
+        warn:function(){}
+      },
+      services:{
+        session:{
+          on:function(){}
+        },
+        error:{
+          handleSystem:function(){},
+          SystemError:function(message){
+            done(new Error(message));
+          }
+        }
+      }
+
+    };
+
+    protocolMock.initialize({
+    }, function(e){
+
+      protocolMock.config.protocols['happn_1'] = {
+        success:function(){
+          done();
+        }
+      }
+
+      protocolMock.processInboundStack = function(message, protocol){
+        expect(message.session.protocol).to.be('happn_1.3.0');
+        return new Promise(function(resolve, reject){
+          return resolve();
+        });
+      };
+
+      protocolMock.processMessageInLayers({
+        session:{
+          protocol:'happn_1.3.0'
+        }
+      }, function(e){
+
+      });
+    });
+  });
+
+  it('tests the processMessageInLayers method, bad protocol', function(done){
+
+    var protocolMock = new Protocol({logger:{
+      createLogger:function(){
+        return {
+          $$TRACE:function(){}
+        };
+      }
+    }});
+
+    protocolMock.happn = {
+      connect:{},
+      log:{
+        warn:function(){}
+      },
+      services:{
+        session:{
+          on:function(){}
+        },
+        error:{
+          SystemError:function(message){
+            expect(message).to.be('unknown inbound protocol: bad');
+            done();
+          }
+        }
+      }
+
+    };
+
+    protocolMock.initialize({
+    }, function(e){
+
+      protocolMock.processMessageInLayers({
         session:{
           protocol:'bad'
         }
