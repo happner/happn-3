7a470ee746dac765e27f0267f0bf073f6cae3e7d
Simon Bishop <simon@tenacious.digital>
2020-01-21 15:20:38 +0200

    fix: issue with node 13 errno in client socket-error test

--- test/integration/client/socket-error.js
+++ test/integration/client/socket-error.js
@@ -1,63 +1,30 @@
-var Promise = require('bluebird');
-var expect = require('expect.js');
-var Happn = require('../../../');
+const expect = require('expect.js');
+const Happn = require('../../../');
 
 describe(
   require('../../__fixtures/utils/test_helper')
     .create()
     .testName(__filename, 3),
   function() {
-    var server;
 
-    var startServer = function(callback) {
-      Happn.service
-        .create()
-        .then(function(_server) {
-          server = _server;
-        })
-        .then(callback)
-        .catch(callback);
-    };
+    this.timeout(4000);
 
-    var stopServerDisconnect = function(callback) {
-      if (!server) return callback();
-      server.stop(
-        {
-          reconnect: false
-        },
-        function(e) {
-          if (e) return callback(e);
-          server = undefined;
-          callback();
-        }
-      );
-    };
-
-    before('start server', startServer);
-    after('stop server', stopServerDisconnect);
-
-    it('fails to login', function(done) {
+    it('fails to login to non existing server', function(done) {
       Promise.resolve()
 
         .then(function() {
-          // other tests may have left the server stopped.
-          if (server) return;
-          return Promise.promisify(startServer)();
-        })
-
-        .then(function() {
-          return Happn.client.create({ port: 55001 });
+          return Happn.client.create({ port: 55555 });
         })
 
         .catch(function(e) {
           expect(e.stack).to.not.be(null);
           expect(e.stack).to.not.be(undefined);
-          expect(e.message).to.be('connect ECONNREFUSED 127.0.0.1:55001');
+          expect(e.message).to.be('connect ECONNREFUSED 127.0.0.1:55555');
           expect(e.code).to.be('ECONNREFUSED');
-          expect(e.errno).to.be('ECONNREFUSED');
+          expect(e.errno === 'ECONNREFUSED' || e.errno < 0).to.be(true);
           expect(e.syscall).to.be('connect');
           expect(e.address).to.be('127.0.0.1');
-          expect(e.port).to.be(55001);
+          expect(e.port).to.be(55555);
           done();
         });
     });
