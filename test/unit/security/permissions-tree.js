d5eabf1780346b96ad1994bfbcbd46dd8a63ef83
Aaron Marais <its_me@aaronleem.co.za>
2021-03-23 18:14:34 +0200

    SMC-1810: test if permission tree works as expected

--- test/unit/security/permissions-tree.js
+++ test/unit/security/permissions-tree.js
@@ -1,33 +1,106 @@
+/* eslint-disable no-console */
 const PermissionsTree = require('../../../lib/services/security/permissions-tree');
 const tests = require('../../__fixtures/utils/test_helper').create();
+
+const expect = require('chai').expect;
+
 describe(tests.testName(__filename, 3), function() {
   it('tests create and search', function() {
     const permissionsTree = PermissionsTree.create(flattenedObjectScenario1());
-    console.log(JSON.stringify(permissionsTree.tree, null, 2))
-    tests.expect(permissionsTree.tree).to.eql(expectedTreeScenario1());
-    tests.expect(permissionsTree.search('/test/permission/1/1/2')).to.eql(searchResultsScenario1());
+    console.log(JSON.stringify(permissionsTree.tree, null, 2));
+    expect(permissionsTree.tree).to.deep.equal(expectedTreeScenario1());
+    expect(permissionsTree.search('/test/permission/1/1/2')).to.deep.equal(
+      searchResultsScenario1()
+    );
   });
 
   it('tests short permission path', function() {
     const permissionsTree = PermissionsTree.create(flattenedObjectScenario2());
-    tests.expect(permissionsTree.tree).to.eql(expectedTreeScenario2());
-    tests.expect(permissionsTree.search('/test/permission/1/1/2')).to.eql(searchResultsScenario2());
+    expect(permissionsTree.tree).to.deep.equal(expectedTreeScenario2());
+    expect(permissionsTree.search('/test/permission/1/1/2')).to.deep.equal(
+      searchResultsScenario2()
+    );
   });
 
   it('tests prohibited permission paths', function() {
     const permissionsTree = PermissionsTree.create(flattenedObjectScenario3());
-    tests.expect(permissionsTree.tree).to.eql(expectedTreeScenario3());
-    tests.expect(permissionsTree.search('/test/permission/1/1/2')).to.eql(searchResultsScenario3());
+    expect(permissionsTree.tree).to.deep.equal(expectedTreeScenario3());
+    expect(permissionsTree.search('/test/permission/1/1/2')).to.deep.equal(
+      searchResultsScenario3()
+    );
   });
 
-
-  it.only('tests building a list from a tree', function() {
+  it('tests building a list from a tree', function() {
     const permissionsTree = PermissionsTree.create(flattenedObjectScenario4());
-    console.log(JSON.stringify(permissionsTree.tree, null, 2))
-    let permissions = permissionsTree.wildcardPathSearch('/test/permission/**', "get");
-    console.log(permissions)
+    console.log(JSON.stringify(permissionsTree.tree, null, 2));
+    let permissions = permissionsTree.wildcardPathSearch('/test/permission/**', 'get');
+    console.log(permissions);
   });
 
+  it("tests if there's a recursive wildcard in the middle of a request", function() {
+    const permissionsTree = PermissionsTree.create(flattenedObjectScenario4());
+    const permissions = permissionsTree.wildcardPathSearch('/test/permission/1/**/3', 'get');
+
+    expect(permissions)
+      .to.be.instanceOf(Error)
+      .with.property('message', 'Recursive wildcards are invalid unless at end of permission path');
+  });
+
+  it('tests that a wildcard in the middle of a permission path returns all children if you have permissions to those', function() {
+    const permissionsTree = PermissionsTree.create(flattenedObjectScenario5());
+    const permissions = permissionsTree.wildcardPathSearch('/test/permission/1/*/3', 'get');
+
+    expect(permissions).to.have.members(['/test/permission/1/2/3', '/test/permission/1/6/3']);
+  });
+
+  it('tests that a wildcard only returns those matched items, no child paths', function() {
+    const permissionsTree = PermissionsTree.create(flattenedObjectScenario6());
+    const permissions = permissionsTree.wildcardPathSearch('/test/permission/1/*', 'get');
+
+    expect(permissions).to.have.members([
+      '/test/permission/1/2',
+      '/test/permission/1/7',
+      '/test/permission/1/5'
+    ]);
+  });
+
+  it('tests that a recursive wildcard only returns those matched items, no child paths', function() {
+    const permissionsTree = PermissionsTree.create(flattenedObjectScenario6());
+    const permissions = permissionsTree.wildcardPathSearch('/test/permission/1/**', 'get');
+
+    expect(permissions).to.have.members([
+      '/test/permission/1/2',
+      '/test/permission/1/7',
+      '/test/permission/1/5',
+      '/test/permission/1/9/5'
+    ]);
+  });
+
+  function flattenedObjectScenario6() {
+    return {
+      '/test/permission/1/2': { actions: ['get'] },
+      '/test/permission/1/6': { prohibit: ['get'] },
+      '/test/permission/1/7': { actions: ['get'] },
+      '/test/permission/1/2/3': { actions: ['get'] },
+      '/test/permission/1/1/3': { prohibit: ['get'] },
+      '/test/permission/1/6/7': { actions: ['get'] },
+      '/test/permission/1/6/3': { actions: ['get'] },
+      '/test/permission/1/5': { actions: ['get'] },
+      '/test/permission/1/5/4': { actions: ['get'] },
+      '/test/permission/1/9/5': { actions: ['get'] }
+    };
+  }
+
+  function flattenedObjectScenario5() {
+    return {
+      '/test/permission/1/2/3': { actions: ['get'] },
+      '/test/permission/1/1/3': { prohibit: ['get'] },
+      '/test/permission/3/4/5': { actions: ['get'] },
+      '/test/permission/1/6/7': { actions: ['get'] },
+      '/test/permission/1/6/3': { actions: ['get'] },
+      '/test/permission/5/6/8': { actions: ['get'] }
+    };
+  }
 
   function flattenedObjectScenario4() {
     return {
