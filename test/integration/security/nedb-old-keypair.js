f9fd7b505387d279126d90d09b72d819f929ace6
Simon Bishop <simon@tenacious.digital>
2022-02-14 13:36:56 +0200

    SMC-5146: fixed old-keypair test path

--- test/integration/security/nedb-old-keypair.js
+++ test/integration/security/nedb-old-keypair.js
@@ -3,11 +3,11 @@ describe(test.testName(__filename, 3), function() {
   it('is able to startup an instance based on an old keypair (bitcore)', async () => {
     const filename = require('path').resolve(
       __dirname,
-      '../../../__fixtures/test/integration/security/old-keypair.nedb'
+      '../../__fixtures/test/integration/security/old-keypair.nedb'
     );
     const originalfilename = require('path').resolve(
       __dirname,
-      '../../../__fixtures/test/integration/security/old-keypair-original.nedb'
+      '../../__fixtures/test/integration/security/old-keypair-original.nedb'
     );
     test.fs.writeFileSync(filename, test.fs.readFileSync(originalfilename));
     const config = {
